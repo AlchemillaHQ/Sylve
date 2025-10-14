642f369b8ec1cc1de240b0eeed0cdd0cf5aeee5d
hayzamjs <hayzam@alchemilla.io>
2025-10-14 22:02:17 +0530

    chore: fix licenses

--- web/src/lib/utils/inet.ts
+++ web/src/lib/utils/inet.ts
@@ -1,3 +1,13 @@
+/**
+ * SPDX-License-Identifier: BSD-2-Clause
+ *
+ * Copyright (c) 2025 The FreeBSD Foundation.
+ *
+ * This software was developed by Hayzam Sherif <hayzam@alchemilla.io>
+ * of Alchemilla Ventures Pvt. Ltd. <hello@alchemilla.io>,
+ * under sponsorship from the FreeBSD Foundation.
+ */
+
 import { Address4, Address6 } from 'ip-address';
 
 export function maskToCIDR(mask: string) {
