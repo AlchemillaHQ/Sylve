9a56e80c74e7f2381d5a141a4207b18a485183ac
hayzamjs <hayzam@alchemilla.io>
2025-11-19 23:53:28 +0530

    i18n: ignore .wuchale, vm: create: fix sw radio button

--- web/src/locales/.wuchale/main.proxy.sync.js
+++
@@ -1,9 +0,0 @@
-
-            import * as _w_c_main_0_ from './main.main.en.compiled.js'
-import * as _w_c_main_1_ from './main.main.mal.compiled.js'
-import * as _w_c_main_2_ from './main.main.hi.compiled.js'
-            /** @type {{[loadID: string]: {[locale: string]: import('wuchale/runtime').CatalogModule}}} */
-            const catalogs = {main: {en: _w_c_main_0_,mal: _w_c_main_1_,hi: _w_c_main_2_}}
-            export const loadCatalog = (/** @type {string} */ loadID, /** @type {string} */ locale) => catalogs[loadID][locale]
-            export const loadIDs = ['main']
-        
