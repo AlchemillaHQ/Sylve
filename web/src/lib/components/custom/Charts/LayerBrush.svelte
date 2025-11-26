156ed2afd2053aba2582143f111fdfceed37c7cc
Charlie Root <ajasmaprince@gmail.com>
2025-11-26 21:58:30 +0530

    layerBrush: heading added

--- web/src/lib/components/custom/Charts/LayerBrush.svelte
+++ web/src/lib/components/custom/Charts/LayerBrush.svelte
@@ -11,66 +11,96 @@
 		Points
 	} from 'layerchart';
 	import { curveBasis } from 'd3-shape';
+	import * as Card from '$lib/components/ui/card/index.js';
 
 	interface Props {
 		points: { date: Date; value: number }[];
 		maxY?: number;
 		label?: string;
 		showPoints: boolean;
+		icon?: string;
+		containerClass?: string;
+		description?: string;
 	}
 
-	let { points, maxY, label = 'Value', showPoints = false }: Props = $props();
+	let {
+		points,
+		maxY,
+		label = 'Value',
+		showPoints = false,
+		icon = '',
+		containerClass = 'p-5',
+		description = ''
+	}: Props = $props();
 </script>
 
-<div class="grid gap-1 rounded-sm border p-4">
-	<State initial={[null, null]} let:value={xDomain} let:set>
-		<div class="h-[300px]">
-			<Chart
-				data={points}
-				x="date"
-				{xDomain}
-				y="value"
-				yDomain={[0, maxY ?? null]}
-				padding={{ left: 16, bottom: 24 }}
-				brush={{
-					resetOnEnd: true,
-					onBrushEnd: (e) => {
-						// @ts-expect-error
-						set(e.xDomain);
-					}
-				}}
-				tooltip={{ mode: 'quadtree-x' }}
-			>
-				<Layer type="svg">
-					<Axis placement="left" grid rule />
-					<Axis placement="bottom" rule />
-					<ChartClipPath>
-						<LinearGradient class="from-primary/50 to-primary/1" vertical>
-							{#snippet children({ gradient })}
-								<Area
-									line={{ class: 'stroke-2 stroke-primary' }}
-									fill={gradient}
-									curve={curveBasis}
-									motion={'tween'}
-								/>
+<Card.Root class={containerClass}>
+	<Card.Header class="p-0">
+		<Card.Title class="flex items-center justify-between gap-4">
+			<div class="flex items-center gap-2">
+				{#if icon}
+					<span class={icon}></span>
+				{/if}
+				{label}
+			</div>
+		</Card.Title>
+		{#if description}
+			<Card.Description>{description}</Card.Description>
+		{/if}
+	</Card.Header>
+
+	<Card.Content class="h-full min-h-[300px] w-full p-0">
+		<div class="grid gap-1 rounded-sm border p-4">
+			<State initial={[null, null]} let:value={xDomain} let:set>
+				<div class="h-[300px]">
+					<Chart
+						data={points}
+						x="date"
+						{xDomain}
+						y="value"
+						yDomain={[0, maxY ?? null]}
+						padding={{ left: 16, bottom: 24 }}
+						brush={{
+							resetOnEnd: true,
+							onBrushEnd: (e) => {
+								// @ts-expect-error
+								set(e.xDomain);
+							}
+						}}
+						tooltip={{ mode: 'quadtree-x' }}
+					>
+						<Layer type="svg">
+							<Axis placement="left" grid rule />
+							<Axis placement="bottom" rule />
+							<ChartClipPath>
+								<LinearGradient class="from-primary/50 to-primary/1" vertical>
+									{#snippet children({ gradient })}
+										<Area
+											line={{ class: 'stroke-2 stroke-primary' }}
+											fill={gradient}
+											curve={curveBasis}
+											motion={'tween'}
+										/>
+									{/snippet}
+								</LinearGradient>
+							</ChartClipPath>
+
+							{#if showPoints}
+								<Points motion={'tween'} r={3} />
+							{/if}
+						</Layer>
+
+						<Tooltip.Root>
+							{#snippet children({ data })}
+								<Tooltip.Header value={data.date} format="daytime" />
+								<Tooltip.List>
+									<Tooltip.Item {label} value={Number(data.value).toFixed(2)} />
+								</Tooltip.List>
 							{/snippet}
-						</LinearGradient>
-					</ChartClipPath>
-
-					{#if showPoints}
-						<Points motion={'tween'} r={3} />
-					{/if}
-				</Layer>
-
-				<Tooltip.Root>
-					{#snippet children({ data })}
-						<Tooltip.Header value={data.date} format="daytime" />
-						<Tooltip.List>
-							<Tooltip.Item {label} value={Number(data.value).toFixed(2)} />
-						</Tooltip.List>
-					{/snippet}
-				</Tooltip.Root>
-			</Chart>
+						</Tooltip.Root>
+					</Chart>
+				</div>
+			</State>
 		</div>
-	</State>
-</div>
+	</Card.Content>
+</Card.Root>
