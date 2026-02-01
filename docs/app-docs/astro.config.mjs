0d1a04822a128b99f23135c25f23a66d60e9e20e
hayzam <hayzam@alchemilla.io>
2026-02-02 02:04:36 +0530

    starlight: reinit

--- docs/app-docs/astro.config.mjs
+++ docs/app-docs/astro.config.mjs
@@ -1,12 +1,9 @@
 // @ts-check
-import { defineConfig, passthroughImageService } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-  	image: {
-    		service: passthroughImageService()
-  	},
 	integrations: [
 		starlight({
 			title: 'My Docs',
