314406bb692ce0de98d6dc93947b5cbc6a06f16f
hayzamjs <hayzam@gmail.com>
2026-03-26 19:09:26 +0100

    chore: fix licenses

--- scripts/add_license.sh
+++ scripts/add_license.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 addlicense -f docs/CODE_LICENSE \
-  -ignore internal/assets/web-files/** \
+  -ignore 'internal/assets/web-files/**' \
+  -ignore 'internal/assets/swagger/**' \
   cmd/** internal/** web/src/lib/**/*.ts web/src/lib/*.ts pkg/utils/** pkg/crypto/** pkg/disk/** pkg/network/**
