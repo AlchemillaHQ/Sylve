979f6df0dfd275282178a537beeea29ba06f8ffa
hayzamjs <hayzam@gmail.com>
2026-03-27 16:23:43 +0100

    scripts: fix add_license to not mess with zelta

--- scripts/add_license.sh
+++ scripts/add_license.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
+set -eu
 
 addlicense -f docs/CODE_LICENSE \
   -ignore 'internal/assets/web-files/**' \
   -ignore 'internal/assets/swagger/**' \
-  cmd/** internal/** web/src/lib/**/*.ts web/src/lib/*.ts pkg/utils/** pkg/crypto/** pkg/disk/** pkg/network/**
+  -ignore 'internal/assets/zelta/**' \
+  cmd/** \
+  internal/** \
+  web/src/lib/*.ts \
+  web/src/lib/**/*.ts \
+  pkg/utils/** \
+  pkg/crypto/** \
+  pkg/disk/** \
+  pkg/network/**
