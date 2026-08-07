7c8085dd4188a7aa941d0f5f219380c2dc734fc2
Petar Dimic <petardimic@Petars-MacBook-Pro.local>
2026-08-07 13:42:27 +0200

    ui: header: consolidate notifications/help/sponsor into top bar

--- web/src/lib/components/custom/Header.svelte
+++ web/src/lib/components/custom/Header.svelte
@@ -2,6 +2,7 @@
 	import { page } from '$app/state';
 	import { getJWTClaims, logOut } from '$lib/api/auth';
 	import ReplicationActivity from '$lib/components/custom/DataCenter/Replication/Activity.svelte';
+	import NotificationBell from '$lib/components/custom/Notifications/Bell.svelte';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
 	import * as Sheet from '$lib/components/ui/sheet/index.js';
@@ -220,6 +221,33 @@
 				devFSDisabled={devFSDisabled}
 			/>
 		{/if}
+
+		<div class="flex items-center gap-1">
+			<NotificationBell />
+			<Button
+				size="sm"
+				class="h-6"
+				onclick={() => window.open('https://sylve.io/docs', '_blank')}
+				title="Documentation"
+			>
+				<div class="flex items-center">
+					<span class="icon-[lucide--circle-help] mr-2 h-5 w-5"></span>
+					<span>Help</span>
+				</div>
+			</Button>
+			<Button
+				size="sm"
+				class="h-6"
+				onclick={() => {
+					storage.openAbout = true;
+				}}
+				title="Sponsor"
+			>
+				<div class="flex items-center">
+					<span class="icon-[mdi--heart] h-5 w-5"></span>
+				</div>
+			</Button>
+		</div>
 
 		<div class="flex items-center">
 			<DropdownMenu.Root>
